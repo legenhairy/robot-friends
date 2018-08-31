78a0046e62bab304b51ec925b5d3520a90087298
Harry Zhang <harryzhang100@gmail.com>
2018-08-31 01:49:47 -0700

    passing onsearchchange and searchField through props, minor syntax changes in render

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { setSearchField } from './actions'
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchRobots
+		searchField: state.searchField
 	}
 }
 
@@ -23,27 +23,22 @@ class App extends Component {
     constructor() {
     	super()
     	this.state = {
-    	  robots: [],
-	      searchfield: ''
-		}
+    	  robots: []
+	    }
 	}		
 	
 
 	componentDidMount() {
-		console.log(this.props.store)
 		fetch('https://jsonplaceholder.typicode.com/users')
 		   .then(response => response.json())
   		   .then(users => this.setState({robots: users}));
 	}
 	
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value});
-	}
-
 	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = this.state.robots.filter(robots =>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
+		const filteredRobots = robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 		if (this.state.robots.length === 0) { //if no robots are passed in 
 			return <h1>Loading</h1>
@@ -51,7 +46,7 @@ class App extends Component {
 		  return (
 		    <div className = "tc"> 
 			   <h1>RoboFriends</h1>	
-			   <SearchBox searchChange={this.onSearchChange} />
+			   <SearchBox searchChange={onSearchChange} />
 			   <Scroll>
 			     <ErrorBoundry>	
 			       <CardList robots={filteredRobots} />
